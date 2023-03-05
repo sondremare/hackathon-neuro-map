c8ab6581d607ac5b1b65be0ebf1ee5b02167478a
Esten Leonardsen <estenleonardsen@gmail.com>
2023-03-05 22:14:45 +0100

    Fixed camera perspectives

--- src/App.js
+++ src/App.js
@@ -13,107 +13,87 @@ import '@kitware/vtk.js/IO/Core/DataAccessHelper/JSZipDataAccessHelper';
 import vtkImageMapper from '@kitware/vtk.js/Rendering/Core/ImageMapper';
 import vtkImageData from "@kitware/vtk.js/Common/DataModel/ImageData";
 import vtkImageSlice from '@kitware/vtk.js/Rendering/Core/ImageSlice';
-import ControlPanel from "./ControlPanel";
+import vtkGenericRenderWindow from "@kitware/vtk.js/Rendering/Misc/GenericRenderWindow";
 import NiftiReader from "nifti-reader-js";
 import vtkDataArray from "@kitware/vtk.js/Common/Core/DataArray";
-import {setUpView} from "./setUpView";
+import {Row, Col, Container, RangeSlider} from "react-bootstrap";
+import ReactBootstrapSlider from 'react-bootstrap-slider';
+import FileUpload from "./FileUpload";
+
+const setUpView = (ref, actors, background=[0.0, 0.0, 0.0]) => {
+  const genericScreenRender = vtkGenericRenderWindow.newInstance({
+      background: background,
+  });
+
+  genericScreenRender.setContainer(ref.current);
+
+  const renderWindow = genericScreenRender.getRenderWindow();
+  const renderer = genericScreenRender.getRenderer();
+
+  for (const actor of actors) {
+      renderer.addActor(actor);
+  }
+
+  return {
+      renderWindow,
+      renderer
+  }
+}
 
 function App() {
-  const sagitalContainerRef = useRef(null);
+  const sagittalContainerRef = useRef(null);
   const axialContainerRef = useRef(null);
   const coronalContainerRef = useRef(null);
   const vtkContainerRef = useRef(null);
-  const context = useRef(null);
-  const [imageData, setImageData] = useState(null);
 
-  const readNifty = (name, file) => {
+  const context = useRef(null);
+
+  const [imageData, setImageData] = useState(null);
+  const [focalPoint, setFocalPoint] = useState(null);
+  const [dimensions, setDimensions] = useState(null);
+
+  const readNifti = (name, file) => {
     const data = NiftiReader.isCompressed(file) ? NiftiReader.decompress(file) : file;
 
     if (!NiftiReader.isNIFTI(data)) {
-      alert('not nifty data breh!');
+      alert('Not a nifti file');
     }
 
     const header = NiftiReader.readHeader(data);
-    console.log('heaader', header);
     const niftiImage = NiftiReader.readImage(header, data);
-
-    console.log('niftiImage', niftiImage);
 
     const dataArray = vtkDataArray.newInstance({
       numberOfComponents: 1,
       values: new Uint8Array(niftiImage),
     });
 
-    console.log('dataArray - range', dataArray.getRange());
-
-
     const localImageData = vtkImageData.newInstance()
     localImageData.getPointData().setScalars(dataArray);
     localImageData.setDimensions(header.dims.slice(1, 4));
-    // localImageData.setDimensions([167, 212, 160]); // TODO: SET THE DIMENSIONS LIKE A NON_NNUUB
-
-    console.log('localImageData', localImageData.toJSON());
-    console.log('extent', localImageData.getExtent());
-    console.log('dims', localImageData.getDimensions());
 
     setImageData(localImageData);
+    setDimensions(header.dims.slice(1, 4))
   };
 
-  // Show full 3D view
+  // Setup views
   useEffect(() => {
-    if (context.current) {
-      const {imageActorI, imageActorJ, imageActorK, sagitalView, fullBlastView, axialView, coronalView} = context.current;
-      const imageMapperK = vtkImageMapper.newInstance();
-      imageMapperK.setInputData(imageData);
-      imageMapperK.setKSlice(30);
-      imageActorK.setMapper(imageMapperK);
-      console.log('YAY');
-
-      const imageMapperJ = vtkImageMapper.newInstance();
-      imageMapperJ.setInputData(imageData);
-      imageMapperJ.setJSlice(30);
-      imageActorJ.setMapper(imageMapperJ);
-      //
-      const imageMapperI = vtkImageMapper.newInstance();
-      imageMapperI.setInputData(imageData);
-      imageMapperI.setISlice(30);
-      imageActorI.setMapper(imageMapperI);
-      //
-      sagitalView.renderer.resetCamera();
-      fullBlastView.renderer.resetCamera();
-      axialView.renderer.resetCamera();
-      coronalView.renderer.resetCamera();
-
-      sagitalView.renderer.resetCameraClippingRange();
-      fullBlastView.renderer.resetCameraClippingRange();
-      axialView.renderer.resetCameraClippingRange();
-      coronalView.renderer.resetCameraClippingRange();
-
-      sagitalView.renderWindow.render();
-      fullBlastView.renderWindow.render();
-      axialView.renderWindow.render();
-      coronalView.renderWindow.render();
-
-    }
-  }, [imageData]);
-
-  // setup 3d view
-  useEffect(() => {
-    if (vtkContainerRef && sagitalContainerRef && axialContainerRef && coronalContainerRef) {
+    if (vtkContainerRef && sagittalContainerRef && axialContainerRef && coronalContainerRef) {
       if (!context.current) {
 
         const imageActorI = vtkImageSlice.newInstance();
         const imageActorJ = vtkImageSlice.newInstance();
         const imageActorK = vtkImageSlice.newInstance();
 
-        const sagitalView = setUpView(sagitalContainerRef, [imageActorI]);
+        const sagittalView = setUpView(sagittalContainerRef, [imageActorI]);
         const axialView = setUpView(axialContainerRef, [imageActorJ]);
         const coronalView = setUpView(coronalContainerRef, [imageActorK]);
 
-        const fullBlastView = setUpView(vtkContainerRef, [imageActorI, imageActorJ, imageActorK]);
+        const fullBlastView = setUpView(vtkContainerRef,
+                                        [imageActorI, imageActorJ, imageActorK],
+                                        [1.0, 1.0, 1.0]);
 
         context.current = {
-          sagitalView,
+          sagittalView,
           axialView,
           coronalView,
           fullBlastView,
@@ -123,33 +103,110 @@ function App() {
         };
       }
     }
+  }, [vtkContainerRef, sagittalContainerRef, axialContainerRef, coronalContainerRef]);
 
-    return () => {
+  // Render views
+  useEffect(() => {
+    if (context.current) {
+      const {imageActorI, imageActorJ, imageActorK, sagittalView, fullBlastView, axialView, coronalView} = context.current;
+      const imageMapperK = vtkImageMapper.newInstance();
+      imageMapperK.setInputData(imageData);
+      imageActorK.setMapper(imageMapperK);
 
-    };
-    // return () => {
-    //   if (context.current) {
-    //     const { fullScreenRenderer, actor, mapper } = context.current;
-    //     actor.delete();
-    //     mapper.delete();
-    // fullScreenRenderer.delete();
-    // context.current = null;
-    // }
-    // };
-  }, [vtkContainerRef, sagitalContainerRef, axialContainerRef, coronalContainerRef]);
+      const imageMapperJ = vtkImageMapper.newInstance();
+      imageMapperJ.setInputData(imageData);
+      imageActorJ.setMapper(imageMapperJ);
+      //
+      const imageMapperI = vtkImageMapper.newInstance();
+      imageMapperI.setInputData(imageData);
+      imageActorI.setMapper(imageMapperI);
+
+      sagittalView.renderer.resetCamera();
+      fullBlastView.renderer.resetCamera();
+      axialView.renderer.resetCamera();
+      coronalView.renderer.resetCamera();
+
+      setFocalPoint([84.5, 105, 80])
+
+      context.current = {
+        sagittalView,
+        axialView,
+        coronalView,
+        fullBlastView,
+        imageActorI,
+        imageActorJ,
+        imageActorK,
+        imageMapperI,
+        imageMapperJ,
+        imageMapperK
+      };
+    }
+  }, [imageData]);
+
+  useEffect (() => {
+    if (context.current && focalPoint) {
+      const [k, j, i] = focalPoint
+      const {imageMapperI, imageMapperJ, imageMapperK, sagittalView, fullBlastView, axialView, coronalView} = context.current;
+      imageMapperK.setKSlice(k);
+      imageMapperJ.setJSlice(j);
+      imageMapperI.setISlice(i);
+
+      sagittalView.renderer.getActiveCamera().setViewUp(0, 0, 1)
+      sagittalView.renderer.getActiveCamera().setFocalPoint(k, j, i)
+      sagittalView.renderer.getActiveCamera().setPosition(k+505, j, i)
+
+      axialView.renderer.getActiveCamera().setFocalPoint(k, j, i)
+      axialView.renderer.getActiveCamera().setPosition(k, j + 485, i)
+
+      coronalView.renderer.getActiveCamera().setFocalPoint(k, j, i)
+      coronalView.renderer.getActiveCamera().setPosition(k, j, i + 520)
+
+      sagittalView.renderWindow.render();
+      fullBlastView.renderWindow.render();
+      axialView.renderWindow.render();
+      coronalView.renderWindow.render();
+    }
+  }, [focalPoint])
+
+  //var maxI = 256
+  //var maxJ = 256
+  //var maxK = 256
+
+  //if (dimensions) {
+  //  maxI = dimensions[0]
+  //  maxJ = dimensions[1]
+  //  maxK = dimensions[2]
+  //}
 
   return (
-      <div style={{display: 'flex', flexDirection: 'column'}}>
-        <div style={{display: 'flex'}}>
-          <div ref={sagitalContainerRef} style={{height: 500, width: 500}}/>
-          <div ref={axialContainerRef} style={{height: 500, width: 500}}/>
-        </div>
-        <div style={{display: 'flex'}}>
-          <div ref={coronalContainerRef} style={{height: 500, width: 500}}/>
-          <div ref={vtkContainerRef} style={{height: 500, width: 500}}/>
-        </div>
-        <ControlPanel onUpload={readNifty}/>
-      </div>
+    <Container>
+      <Row>
+        <FileUpload onUpload={readNifti}/>
+      </Row>
+      <Row>
+        <Col>
+          <div ref={sagittalContainerRef} style={{height: 250, width: 250}}/>
+          <ReactBootstrapSlider
+            min={0}
+          />
+        </Col>
+        <Col>
+          <div ref={axialContainerRef} style={{height: 250, width: 250}}/>
+          <ReactBootstrapSlider
+            min={0}
+          />
+        </Col>
+        <Col>
+          <div ref={coronalContainerRef} style={{height: 250, width: 250}}/>
+          <ReactBootstrapSlider
+            min={0}
+          />
+        </Col>
+        <Col>
+          <div ref={vtkContainerRef} style={{height: 250, width: 250}}/>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-slider/dist/css/bootstrap-slider.css"
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
